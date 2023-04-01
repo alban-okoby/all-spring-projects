3484c9a709e56992b477ac6a2dd8a0b548e00503
Cerveau++ <77627170+alban-okoby@users.noreply.github.com>
2023-04-01 17:29:48 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# all-spring-projects
+ Regroup here, all of spring like spring security, spring mvc, spring cloud .. 💻<\☕>
---
+++ spring-security/src/test/java/com/digitalsouag/security/SecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.digitalsouag.security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
