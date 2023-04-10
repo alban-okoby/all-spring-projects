ed27994ea16a1361254544f5944b8cdd09f11889
Cerveau++ <77627170+alban-okoby@users.noreply.github.com>
2023-04-10 15:13:17 +0000

    Init reddit clone projet

---
+++ spring-reddit-clone-api/src/main/java/com/digitalsouag/redditapi/RedditapiApplication.java
@@ -0,0 +1,13 @@
+package com.digitalsouag.redditapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedditapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RedditapiApplication.class, args);
+	}
+
+}
